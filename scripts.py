a9ed86b8322f0e9d609caa410827f6472588ed0a
frqhero <frqhero@gmail.com>
2023-05-18 13:23:04 +0600

    Have blue applied to the code

--- scripts.py
+++ scripts.py
@@ -1,75 +1,72 @@
-from datacenter.models import (
-	Schoolkid, Lesson, Commendation, Subject
-)
+from datacenter.models import Schoolkid, Lesson, Commendation, Subject
 
 
 class HackError(Exception):
-	def __init__(self, message):
-		self.message = message
+    def __init__(self, message):
+        self.message = message
 
 
 class Hack:
-	def __init__(
-			self,
-			fio='Фролов Иван',
-			subject_name='Математика',
-			achievement='Отлично!'
-	):
-		self.fio = fio
-		self.subject_name = subject_name
-		self.achievement = achievement
-		self.set_student()
-		self.set_subject()
+    def __init__(
+        self,
+        fio='Фролов Иван',
+        subject_name='Математика',
+        achievement='Отлично!',
+    ):
+        self.student = self.get_student(fio)
+        self.subject = self.get_subject(subject_name)
+        self.achievement = achievement
 
-	def set_student(self):
-		if self.fio == '':
-			raise HackError('Пустое имя')
-		try:
-			self.student = Schoolkid.objects.get(full_name__contains=self.fio)
-		except Schoolkid.DoesNotExist:
-			raise HackError('По указанному имени не найдено учеников')
-		except Schoolkid.MultipleObjectsReturned:
-			raise HackError(
-				'По указанному имени найдено более одного ученика'
-			)
+    @staticmethod
+    def get_student(fio):
+        if fio == '':
+            raise HackError('Пустое имя')
+        try:
+            return Schoolkid.objects.get(full_name__contains=fio)
+        except Schoolkid.DoesNotExist:
+            raise HackError('По указанному имени не найдено учеников')
+        except Schoolkid.MultipleObjectsReturned:
+            raise HackError('По указанному имени найдено более одного ученика')
 
-	def set_subject(self):
-		try:
-			self.subject = Subject.objects.get(
-				title=self.subject_name,
-				year_of_study=self.student.year_of_study
-			)
-		except Subject.DoesNotExist:
-			raise HackError('Не найдено предмета у указанного ученика')
-		except Subject.MultipleObjectsReturned:
-			raise HackError(
-				'По указанному названию найдено более одного предмета'
-			)
+    def get_subject(self, subject_name):
+        try:
+            return Subject.objects.get(
+                title=subject_name,
+                year_of_study=self.student.year_of_study,
+            )
+        except Subject.DoesNotExist:
+            raise HackError('Не найдено предмета у указанного ученика')
+        except Subject.MultipleObjectsReturned:
+            raise HackError(
+                'По указанному названию найдено более одного предмета'
+            )
 
-	def fix_marks(self):
-		self.student.mark_set.filter(points__in=[2, 3]).update(
-			points=5
-		)
+    def fix_marks(self):
+        self.student.mark_set.filter(points__in=[2, 3]).update(points=5)
 
-	def remove_chastisements(self):
-		self.student.chastisement_set.all().delete()
+    def remove_chastisements(self):
+        self.student.chastisement_set.all().delete()
 
-	def create_commendation(self):
-		lesson = Lesson.objects.filter(
-			subject__title=self.subject_name,
-			year_of_study=self.student.year_of_study,
-			group_letter=self.student.group_letter
-		).order_by('-date').first()
-		commendation = Commendation.objects.create(
-			teacher=lesson.teacher,
-			subject=lesson.subject,
-			created=lesson.date,
-			schoolkid=self.student,
-			text=self.achievement
-		)
-		commendation.save()
+    def create_commendation(self):
+        lesson = (
+            Lesson.objects.filter(
+                subject__title=self.subject.title,
+                year_of_study=self.student.year_of_study,
+                group_letter=self.student.group_letter,
+            )
+            .order_by('-date')
+            .first()
+        )
+        commendation = Commendation.objects.create(
+            teacher=lesson.teacher,
+            subject=lesson.subject,
+            created=lesson.date,
+            schoolkid=self.student,
+            text=self.achievement,
+        )
+        commendation.save()
 
-	def carry_out_all_features(self):
-		self.fix_marks()
-		self.remove_chastisements()
-		self.create_commendation()
+    def carry_out_all_features(self):
+        self.fix_marks()
+        self.remove_chastisements()
+        self.create_commendation()
